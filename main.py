6e403d8a9805b26a310da552a913cfca3966d70b
Lawrence Babb <lbabb@broadinstitute.org>
2022-02-22 09:14:33 -0500

    styling and minor clean-up

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import io
 
 from flask import escape
 from jsonschema import validate
-from confluent_kafka import Producer, KafkaError
+from confluent_kafka import Producer
 
 delivered_records = 0
 
@@ -21,43 +21,44 @@ client.setup_logging()
 
 # Describe what kind of json you expect.
 releasePayloadSchema = {
-  "type": "object",
-  "properties": {
-    "action": {"type": "string"},
-    "release": {
-      "type": "object",
-      "properties": {
-        "tag_name": {"type": "string"},
-        "name": {"type": "string"},
-        "draft": {"type": "boolean"},
-        "prerelease": {"type": "boolean"},
-        "created_at": {"type": "string", "format": "date-time"},
-        "published_at": {"type": "string", "format": "date-time"},
-        "assets": {
-          "type": "array",
-          "items": {
-            "type":"object",
+    "type": "object",
+    "properties": {
+        "action": {"type": "string"},
+        "release": {
+            "type": "object",
             "properties": {
-              "name": {"type": "string"},
-              "browser_download_url": {"type": "string"}
+                "tag_name": {"type": "string"},
+                "name": {"type": "string"},
+                "draft": {"type": "boolean"},
+                "prerelease": {"type": "boolean"},
+                "created_at": {"type": "string", "format": "date-time"},
+                "published_at": {"type": "string", "format": "date-time"},
+                "assets": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "name": {"type": "string"},
+                            "browser_download_url": {"type": "string"}
+                        },
+                        "required": ["name", "browser_download_url"]
+                    }
+                }
             },
-            "required": ["name","browser_download_url"]
-          }
+            "required": ["name", "draft", "prerelease", "published_at", "assets"]
+        },
+        "repository": {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "full_name": {"type": "string"}
+            },
+            "required": ["name", "full_name"]
         }
-      },
-      "required": ["name","draft","prerelease","published_at","assets"]
     },
-    "repository": {
-      "type": "object",
-      "properties": {
-        "name": {"type": "string"},
-        "full_name": {"type": "string"}
-      },
-      "required": ["name","full_name"]
-    }
-  },
-  "required": ["action","release","repository"]
+    "required": ["action", "release", "repository"]
 }
+
 
 def validate_signature(request):
 
@@ -65,28 +66,30 @@ def validate_signature(request):
     signature_header = request.headers.get('X-Hub-Signature-256')
     sha_name, github_signature = signature_header.split('=')
     if sha_name != 'sha256':
-      logging.error('X-Hub-Signature in payload headers was not sha256=****')
-      return False
+        logging.error('X-Hub-Signature in payload headers was not sha256=****')
+        return False
 
     # Create our own signature
     git_webhook_secret = os.environ.get('GIT_MONDO_WEBHOOK_SECRET')
-    local_signature = hmac.new(git_webhook_secret.encode('utf-8'), msg=request.data, digestmod=hashlib.sha256)
+    local_signature = hmac.new(git_webhook_secret.encode(
+        'utf-8'), msg=request.data, digestmod=hashlib.sha256)
 
     # See if they match
     return hmac.compare_digest(local_signature.hexdigest(), github_signature)
 
+
 def validateJson(jsonData):
     try:
-      validate(instance=jsonData, schema=releasePayloadSchema)
+        validate(instance=jsonData, schema=releasePayloadSchema)
     except jsonschema.exceptions.ValidationError as err:
-      logging.error("Payload schema error: '{}'".format(err))
-      return False
+        logging.error("Payload schema error: '{}'".format(err))
+        return False
     return True
+
 
 def init_event_configs(release_json):
     event_cfgs_str = os.environ.get('MONDO_RELEASE_EVENTS')
     event_cfgs = json.loads(event_cfgs_str)
-    # pprint(event_cfgs)
 
     for event_cfg in event_cfgs:
         for assets in release_json['assets']:
@@ -97,33 +100,32 @@ def init_event_configs(release_json):
                     "release_tag": release_json['tag_name'],
                     "release_date": release_json['published_at']}
                 event_cfg.update(y)
-                # pprint(event_cfg)
 
     return event_cfgs
 
+
 def get_event_records(event_cfg):
     r = requests.get(event_cfg['browser_download_url'])
-    # pprint(r.status_code)
     r.encoding = 'utf-8'
-    # pprint(r.encoding)
     content = r.text
 
     # Reading the downloaded content and turning it into a pandas dataframe
-    df = pd.read_csv(io.StringIO(content), sep='\t').fillna(np.nan).replace([np.nan], [None])
-    # print('\t'+ df.head(5).to_string().replace('\n', '\n\t'))
+    df = pd.read_csv(io.StringIO(content), sep='\t').fillna(
+        np.nan).replace([np.nan], [None])
 
     # if this is true then there are duplicates for this set (log warning that only the first is kept)
     if df.duplicated(subset=event_cfg['pk']).agg(max):
-        logging.warning("Duplicate records in "+event_cfg['asset_name'])
-        # print("Duplicate records in "+event_cfg['asset_name'])
+        logging.warning("Duplicate records in " + event_cfg['asset_name'])
         # keeps only the first unique row for each group.
         df = df.groupby(event_cfg['pk']).head(1)
 
-    return df.to_dict (orient='records')
+    return df.to_dict(orient='records')
 
 # Optional per-message on_delivery handler (triggered by poll() or flush())
 # when a message has been successfully delivered or
 # permanently failed delivery (after retries).
+
+
 def acked(err, msg):
     global delivered_records
     """Delivery report handler called on
@@ -131,11 +133,11 @@ def acked(err, msg):
     """
     if err is not None:
         logging.error("Failed to deliver message: {}".format(err))
-        # print("Failed to deliver message: {}".format(err))
     else:
         delivered_records += 1
-        # print("Produced record to topic {} partition [{}] @ offset {}"
-        #       .format(msg.topic(), msg.partition(), msg.offset()))
+        logging.info("Produced record to topic {} partition [{}] @ offset {}"
+              .format(msg.topic(), msg.partition(), msg.offset()))
+
 
 def publish_events(producer, event_cfg, event_recs):
     global delivered_records
@@ -148,16 +150,17 @@ def publish_events(producer, event_cfg, event_recs):
             "event_type": event_cfg['name'],
             "content": rec}
         record_value = json.dumps(msg).encode('utf8')
-        # logging.info("Producing record: {}".format(record_value))
-        producer.produce(event_cfg['topic'], value=record_value, on_delivery=acked)
-        # # p.poll() serves delivery reports (on_delivery)
-        # # from previous produce() calls.
+        producer.produce(event_cfg['topic'],
+                         value=record_value, on_delivery=acked)
+        # p.poll() serves delivery reports (on_delivery)
+        # from previous produce() calls.
         producer.poll(0)
 
     producer.flush()
 
-    logging.info("{} '{}' messages were produced to topic {}!".format(delivered_records, event_cfg['name'], event_cfg['topic']))
-    # print("{} messages were produced to topic {}!".format(delivered_records, event_cfg['topic']))
+    logging.info("{} '{}' messages were produced to topic {}!".format(
+        delivered_records, event_cfg['name'], event_cfg['topic']))
+
 
 def process_release(release_json):
     dx_servers = os.environ.get('DX_BOOTSTRAP_SERVERS')
@@ -165,11 +168,11 @@ def process_release(release_json):
     dx_producer_creds = json.loads(dx_producer_creds_str)
 
     conf = {
-        "bootstrap.servers" : dx_servers,
-        "security.protocol" : "SASL_SSL",
-        "sasl.mechanisms" : "PLAIN",
-        "sasl.username" : dx_producer_creds["user"],
-        "sasl.password" : dx_producer_creds["password"]
+        "bootstrap.servers": dx_servers,
+        "security.protocol": "SASL_SSL",
+        "sasl.mechanisms": "PLAIN",
+        "sasl.username": dx_producer_creds["user"],
+        "sasl.password": dx_producer_creds["password"]
     }
     producer = Producer(conf)
 
@@ -178,6 +181,7 @@ def process_release(release_json):
     for event_cfg in events_cfg:
         event_recs = get_event_records(event_cfg)
         publish_events(producer, event_cfg, event_recs)
+
 
 def mondo_notifier(request):
     """HTTP Cloud Function.
@@ -191,29 +195,29 @@ def mondo_notifier(request):
     """
 
     if not request:
-      logging.error("No request object.")
-      return ('No request object. Processing stopped.', 400)
+        logging.error("No request object.")
+        return ('No request object. Processing stopped.', 400)
 
     if not validate_signature(request):
-      logging.error("Github signature did not validate.")
-      return ('Webhook signature did not validate. Processing stopped.', 401)
+        logging.error("Github signature did not validate.")
+        return ('Webhook signature did not validate. Processing stopped.', 401)
 
     request_json = request.get_json(silent=True)
     if not request_json:
-      logging.error("No json payload in request.")
-      return ('No json payload in request. Processing stopped.', 400)
+        logging.error("No json payload in request.")
+        return ('No json payload in request. Processing stopped.', 400)
 
     if "action" not in request_json:
-      logging.error("Json request did not contain expected 'action' key.")
-      return ("Json request did not contain expected 'action' key. Processing stopped.", 400)
+        logging.error("Json request did not contain expected 'action' key.")
+        return ("Json request did not contain expected 'action' key. Processing stopped.", 400)
 
     action = request_json['action']
     if action != "released":
-      return ("Not a 'action=released' request. Ignore request.", 200)
+        return ("Not a 'action=released' request. Ignore request.", 200)
 
     if not validateJson(request_json):
-      logging.info(request_json)
-      return ("Request json did not validate against released payload schema. Processing stopped.", 400)
+        logging.info(request_json)
+        return ("Request json did not validate against released payload schema. Processing stopped.", 400)
 
     logging.info("Action is '{}'. Starting to process files.".format(action))
     process_release(request_json["release"])
